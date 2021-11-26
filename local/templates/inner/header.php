aa60840fbbb66a62f94b32e4b1da24962f06f0de
LAPTOP-D4K0JG6I\ttimu <tpasichnik123@mail.ru>
2021-11-26 12:07:57 +0200

    second commit

---
+++ local/templates/inner/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <?php $APPLICATION->ShowHead(); ?>
+    <title>Yanicode :: Главная</title>
+    <link rel="stylesheet" href="/local/templates/inner/assets/template_styles.css">
+</head>
+<body>
+<div id="panel"><?$APPLICATION->ShowPanel();?></div>
+<header class="header">
+    <div class="container">
+        <div class="header-wrapper">
+            <a href="./index.html" class="header__logo">
+                <img width="300" height="92" src="/local/templates/inner/assets/images/svg/logo-yanicode.svg" alt="yanicode">
+            </a>
+            <div class="header__burger header__burger_close">
+                <span class="burger-line"></span>
+                <span class="burger-line"></span>
+                <span class="burger-line"></span>
+            </div>
+            <div class="header-nav">
+                <nav class="nav-list">
+                    <a href="./index.html" class="nav-list__item">ГЛАВНАЯ</a>
+                    <a href="./services.html" class="nav-list__item">УСЛУГИ</a>
+                </nav>
+                <div class="header__phone">
+                    <a href="tel:+79114510616">+79114510616</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
--- numb.php
+++ numb.php
@@ -1,15 +1,9 @@
 <?php
-
 $sum = 0;
-$arr = [];
-for ($i = 1; $i < 1000; $i++) {
-    if ((int)$i % 3 === 0 || (int)$i % 5 === 0) {
-        $sum += $i;
-        array_push($arr, $i);
-    }
-}
-echo $sum;
-echo '<br>';
-
-
-
+for ($x = 0; $x < 1000; $x++) {
+    if ($x % 3 == 0 or $x % 5 == 0) {
+        $sum += $x;
+    };
+};
+echo "$sum"
+?>
